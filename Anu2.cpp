27112534ed4b7e96802698a243f286f146fe56fe
anusiagunalan <33885473+anusiagunalan@users.noreply.github.com>
2018-01-22 10:17:02 +0800

    Add files via upload

---
+++ Anu2.cpp
@@ -0,0 +1,24 @@
+//this is not a complete program. please finish the program. thank you.
+
+
+#include <iostream>
+using namespace std;
+
+int mark[5]={19,10,8,17,9};
+int main()
+{
+	//insert different value to the third element
+	//tukar third element tu dengan value lain
+	mark[3]=9;
+
+	//get user input and insert sub the third element
+	//tukar the third element yang baru ditukar tu dengan input dari user
+	mark[2];
+
+	//print the first element of the array
+	mark[0];
+
+	//print the ith element of the array
+	cin>>mark[1];
+
+}
