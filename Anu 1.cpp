23108e3ec8bcab4180c45d3a2942b8e95ccf4936
anusiagunalan <33885473+anusiagunalan@users.noreply.github.com>
2018-01-22 10:06:17 +0800

    Add files via upload

---
+++ Anu 1.cpp
@@ -0,0 +1,21 @@
+//its just an array example
+//find the sum of all the value inside the array
+#include <iostream>
+using namespace std;
+
+int ary[]={16, 2, 77, 40, 12071};
+
+int n, result=0;
+
+int main()
+{
+
+	for(n=0; n<5; ++n)
+	{
+		result += ary[n];
+	}
+	cout<<result;
+	return 0;
+	
+
+}
