21630373caaf3ea0a74ee1711d3b6a9f274c402d
anusiagunalan <33885473+anusiagunalan@users.noreply.github.com>
2018-01-23 01:38:25 +0800

    Add files via upload

---
+++ String-Exercise-3Anu.cpp
@@ -0,0 +1,26 @@
+//use the string compare function to compare input string with the setup string
+//make a check with the string password(if else)
+
+#include <iostream>
+#include <cstdio>
+
+int main()
+{
+	char strPswd[80];
+
+	cout<<"Enter string: ";
+	//get the strPswd
+	gets(str);
+	
+	//make an if else that check the password(strPswd)
+	//(string compare function (strPswd,"Check")) 
+		
+		cout<<"invalid password "<<endl;
+	
+	 
+		cout<<"Logged in "<<endl;
+
+	
+
+	return 0;
+}
