f9a1c3a07a3690b592b06f2a21f220ba4a93eb70
anusiagunalan <33885473+anusiagunalan@users.noreply.github.com>
2018-01-23 01:30:23 +0800

    Add files via upload

---
+++ String-Exercise-1Anu.cpp
@@ -0,0 +1,17 @@
+//include your name and matric number here
+//display the string below using the string function state below.
+
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+int main()
+{
+	char engkau[10];
+
+	//stringcopy(a,"hello");
+	
+	cout<<engkau<<endl;
+
+	return 0;
+}
