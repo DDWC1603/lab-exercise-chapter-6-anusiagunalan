4684013759e283b4ea4d21e0954dac7512eaeeb1
anusiagunalan <33885473+anusiagunalan@users.noreply.github.com>
2018-01-22 10:17:19 +0800

    Add files via upload

---
+++ Anu3.cpp
@@ -0,0 +1,23 @@
+//dapatkan 10 input dan keluarkan hasil tambahnya.
+//hasilkan program anda dengan menggunakan array
+
+
+int array[10];
+int sum;
+int x;
+{
+
+cout<<"Enter the 10 numbers:"<<endl;
+for(i=0;i<10;i++)
+{
+   cin>>array[x];
+
+   sum=sum+array[x];
+}
+
+
+
+cout<<"the sum of the 10 numbers : "<<sum<<endl;
+
+std::cin.get();
+}
