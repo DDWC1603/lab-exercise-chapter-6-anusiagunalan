eab22bf1291238cd17c31a1ba18059eb82068e99
anusiagunalan <33885473+anusiagunalan@users.noreply.github.com>
2018-01-23 01:39:01 +0800

    Add files via upload

---
+++ String-Exercise-4Anu.cpp
@@ -0,0 +1,21 @@
+//convert string to uppercase
+//complete the fragment code below using the string to uppercase function.
+
+#include <iostream>
+#include <string>
+
+int main()
+{
+	char lowerToUpper[80];
+
+	/*use string copy function here*/(lowerToUpper, "This is a check");
+
+	for(i=0;i<80;i++)
+	{
+		lowerToUpper[i] = /*uppercase function*/(lowerToUpper[i]);
+
+	}
+
+	cout<<lowerToUpper<<endl;
+}
+	
